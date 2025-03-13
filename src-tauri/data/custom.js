a642380f362d0ca8122b11c66f02311f16bfa724
wwwwc <54879495+wwwwc@users.noreply.github.com>
2025-03-13 21:40:45 +0800

    update custom js from pakeplus

--- src-tauri/data/custom.js
+++ src-tauri/data/custom.js
@@ -24,3 +24,25 @@ const hookClick = (e) => {
 }
 
 document.addEventListener('click', hookClick, { capture: true })
+
+// css filter
+document.addEventListener('DOMContentLoaded', () => {
+    const targetNode = document.body
+    // 配置观察选项
+    const config = {
+        childList: true,
+        subtree: true,
+    }
+    const observer = new MutationObserver((mutationsList, observer) => {
+        for (const mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                const element0 = document.querySelector('selector,');
+                if (element0) {
+                    element0.style.display = 'none';
+                }
+            }
+        }
+    })
+    observer.observe(targetNode, config)
+})
+// end css filter
